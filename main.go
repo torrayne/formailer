fee0f136ad2a2b53c63079c1973932ef0b93ad31
Daniel Atwood <daniel@atwood.io>
2021-01-07 19:23:52 -0500

    Getting client and body

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
-	"net/http"
+	"context"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{"Content-Type": "text/plain"},
-		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!",
-		IsBase64Encoded:   false,
+		StatusCode: 200,
+		Body:       "Hello, " + cc.Client.AppTitle + "\n" + request.Body,
 	}, nil
+
+	// return &events.APIGatewayProxyResponse{
+	// 	StatusCode:        200,
+	// 	Headers:           map[string]string{"Content-Type": "text/plain"},
+	// 	MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+	// 	Body:              "Hello, World!",
+	// 	IsBase64Encoded:   false,
+	// }, nil
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
