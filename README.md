bb30513ade5033ee05346f83b2f092272a4f0185
Daniel Atwood <daniel@atwood.io>
2021-01-07 19:30:07 -0500

    Unmarshal body

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # netlify-form-emailer
+
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/5729030b-a610-4165-b127-02133bd0e9bf/deploy-status)](https://app.netlify.com/sites/netlify-emailer/deploys)
--- main.go
+++ main.go
@@ -1,36 +1,28 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	lc, ok := lambdacontext.FromContext(ctx)
-	if !ok {
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var body map[string]interface{}
+	err := json.Unmarshal([]byte(request.Body), &body)
+	if err != nil {
 		return &events.APIGatewayProxyResponse{
-			StatusCode: 503,
-			Body:       "Something went wrong :(",
+			StatusCode: http.StatusBadRequest,
+			Body:       http.StatusText(http.StatusBadRequest),
 		}, nil
 	}
 
-	cc := lc.ClientContext
-
 	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, " + cc.Client.AppTitle + "\n" + request.Body,
+		StatusCode: http.StatusOK,
+		Body:       fmt.Sprintf("Hello, World!\n%+v", body),
 	}, nil
-
-	// return &events.APIGatewayProxyResponse{
-	// 	StatusCode:        200,
-	// 	Headers:           map[string]string{"Content-Type": "text/plain"},
-	// 	MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-	// 	Body:              "Hello, World!",
-	// 	IsBase64Encoded:   false,
-	// }, nil
 }
 
 func main() {
