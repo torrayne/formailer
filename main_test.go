75a41557ea696f2c4e1ce91ff54e2d3408d5d9d3
Daniel Atwood <daniel@atwood.io>
2021-03-01 16:58:31 -0500

    Add test for isSlice

--- main_test.go
+++ main_test.go
@@ -1,1 +1,28 @@
 package formailer
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestIsSlice(t *testing.T) {
+	tests := map[string]bool{
+		"":          false,
+		"not_slice": false,
+		"a slice":   true,
+	}
+
+	for str, expected := range tests {
+		var r bool
+		split := strings.Split(str, " ")
+		if len(split) > 1 {
+			r = isSlice(split)
+		} else {
+			r = isSlice(split[0])
+		}
+
+		if r != expected {
+			t.Errorf("Unexpected result from isSlice. On: %s\nExpected: %t; Got: %t", str, expected, r)
+		}
+	}
+}
